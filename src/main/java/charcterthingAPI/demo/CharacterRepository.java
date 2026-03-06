e0067b75b211a2f3fc61342a6e083dd1f74398fb
IsaiahTTT <itturner@uncg.edu>
2026-03-05 22:46:58 -0500

    CharactorRepository done

--- src/main/java/charcterthingAPI/demo/CharacterRepository.java
+++ src/main/java/charcterthingAPI/demo/CharacterRepository.java
@@ -1,5 +1,18 @@
 package charcterthingAPI.demo;
 
-public class CharacterRepository {
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CharacterRepository extends JpaRepository<Character, Long> {
     
+    List<Character> findByName(String name);
+
+    @Query(value = "SELECT c.* from characters c WHERE c.role = 'Apprentice'", nativeQuery = true)
+    List<Character> findApprenticeRole(String role);
+
+    @Query(value = "SELECT c.* from characters c WHERE c.role = 'Master'", nativeQuery = true)
+    List<Character> findMasterRole(String role);
 }
